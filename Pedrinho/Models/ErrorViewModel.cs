834b2159ff9ff157b63dba710256d6b8f72b105a
Robson Manfroi <robson.manfroi@gmailo.com>
2018-11-21 19:01:41 -0200

    Adicionar arquivos de projeto.

---
+++ Pedrinho/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pedrinho.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
