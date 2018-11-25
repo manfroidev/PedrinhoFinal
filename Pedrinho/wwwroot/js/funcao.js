7f60f348e0b7297173e02baf4f3ab1de04acf28e
Robson Manfroi <robson.manfroi@gmailo.com>
2018-11-25 01:33:43 -0200

    subindo biblioteca js que o professo pedio

---
+++ Pedrinho/wwwroot/js/funcao.js
@@ -0,0 +1,45 @@
+﻿var scene, camera, renderer;
+var geometry, material, mesh;
+
+
+function Executar() {
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 500;
+
+    geometry = new THREE.BoxGeometry(100, 100, 100);
+    material = new THREE.MeshBasicMaterial({ color: 0xD1A963, wireframe: true });
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setClearColor(0x1F5A94);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    
+
+    document.body.appendChild(renderer.domElement)
+
+    
+
+
+
+
+    function animate() {
+
+        requestAnimationFrame(animate);
+
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.02;
+
+        renderer.render(scene, camera);
+
+    }
+    animate();
+}
+
+window.onload = Executar;
+
